cc58b7f7f18883c401316051df7e615eccf03a8a
annguyen23 <an.nguyen.wa@gmail.com>
2021-04-22 04:46:38 -0700

    fix bug

--- src/test/java/IntStackJunit.java
+++ src/test/java/IntStackJunit.java
@@ -1,25 +1,31 @@
 import static org.junit.Assert.*;
 import org.junit.Test;
+import org.junit.Before;
+
 
 import java.io.*;
 
 public class IntStackJunit {
-  private IntStack stack = new IntStack();
+
+  @Before
+  public void setUp() {
+      IntStack stack = new IntStack();
+  }
 
   @Test
     public void getCapacity() {
       assertEquals(stack.getCapacity(), 0);
   }
 
-    @Test
-    public void checkLength() {
-        essertTtrue(stack.isEmpty());
-    }
+  @Test
+  public void checkLength() {
+      essertTtrue(stack.isEmpty());
+  }
 
-    @Test
-    public void checksetCapacity() {
-        stack.setCapacity(3);
-        essertEquals(stack.getCapacity(), 3);
-    }
+  @Test
+  public void checksetCapacity() {
+      stack.setCapacity(3);
+      essertEquals(stack.getCapacity(), 3);
+  }
 
 }
