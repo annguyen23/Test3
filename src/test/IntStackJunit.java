f441d1feb127aaba93e8d4c662e0a3c4a23b6d26
annguyen23 <an.nguyen.wa@gmail.com>
2021-04-22 03:57:45 -0700

    update java

--- src/test/IntStackJunit.java
+++ src/test/IntStackJunit.java
@@ -5,5 +5,22 @@ import org.junit.rules.ErrorCollector;
 import java.io.*;
 
 public class IntStackJunit {
-  
+  private IntStack stack = new IntStack();
+
+  @Test
+    public void getCapacity() {
+      Assert.essertEquals(stack.getCapacity(), 10);
+  }
+
+    @Test
+    public void checkLength() {
+        Assert.essertTtrue(stack.isEmpty());
+    }
+
+    @Test
+    public void checksetCapacity() {
+        stack.setCapacity(3)
+        Assert.essertEquals(stack.getCapacity(), 3);
+    }
+
 }
