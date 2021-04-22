1b473c2ef0d14403b26cf71883fb713c87b066bf
annguyen23 <54456351+annguyen23@users.noreply.github.com>
2021-04-22 04:35:20 -0700

    Create IntStackJunit.java

---
+++ src/test/java/IntStackJunit.java
@@ -0,0 +1,26 @@
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.rules.ErrorCollector;
+
+import java.io.*;
+
+public class IntStackJunit {
+  private IntStack stack = new IntStack();
+
+  @Test
+    public void getCapacity() {
+      Assert.essertEquals(stack.getCapacity(), 0);
+  }
+
+    @Test
+    public void checkLength() {
+        Assert.essertTtrue(stack.isEmpty());
+    }
+
+    @Test
+    public void checksetCapacity() {
+        stack.setCapacity(3)
+        Assert.essertEquals(stack.getCapacity(), 3);
+    }
+
+}
