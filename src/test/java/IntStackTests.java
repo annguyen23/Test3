9fa374f139defd67f96f8834c21effc597ab36c2
annguyen23 <54456351+annguyen23@users.noreply.github.com>
2021-05-05 03:17:05 -0700

    Add comments

--- src/test/java/IntStackTests.java
+++ src/test/java/IntStackTests.java
@@ -8,12 +8,14 @@ public class IntStackTests {
     @Test
     public void getCapacity() {
         IntStack stack = new IntStack();
-        assertEquals(stack.getCapacity(), 3);
+        // the default capacity of the stack should be 10
+        assertEquals(stack.getCapacity(), 10);
     }
 
     @Test
     public void checkLength() {
         IntStack stack = new IntStack();
+        // stack should be empty at this point
         assertTrue(stack.isEmpty());
     }
 
@@ -21,6 +23,9 @@ public class IntStackTests {
     public void checksetCapacity() {
         IntStack stack = new IntStack();
         stack.setCapacity(3);
+        // the initial capacity  is 10, and 
+        // the new capacity must be larger than the previous capacity to able to change it
+        // the capacity should not be changed here
         assertEquals(stack.getCapacity(), 10);
     }
 
